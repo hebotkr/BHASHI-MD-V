ea7a935889f982707796b54b2855190360f1b540
hebotkr <heprabathmdacc@gmail.com>
2025-01-26 20:46:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
   SESSION_ID: process.env.SESSION_ID || "yyh2CBxT#yPJ2b2m_JJ8RoBX3ZenpsHosoFVLZ_Y5x1FWzW_n5gw",
 // For extra thing : BHASHI-MD~(megafileid)
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:EPFIYNcKWdSVoMSaZGhcnFNoKnTryKFY@junction.proxy.rlwy.net:34409",
 
     
  
